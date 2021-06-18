564e7d11c53796d34aa05476aa6f9c000b6e729f
Alejandro Rangel <buffalem@gmail.com>
2021-06-18 10:52:32 -0500

    AddAlumnoInterfaz completo y actualización de modelos

--- src/main/java/com/poo/models/Universidad.java
+++ src/main/java/com/poo/models/Universidad.java
@@ -9,6 +9,12 @@ public class Universidad {
     private ArrayList<Docente> docentes = new ArrayList<>();;
     private ArrayList<PersonalApoyo> personalApoyo =  new ArrayList<>() ;
     private ArrayList<Materia> materias = new ArrayList<>();
+    private static int alumnosId = 1;
+    private static int docentesId = 1;
+    private static int personalApoyoId = 1;
+    private static int materiasId = 1;
+
+
 
     public Universidad(){
 
@@ -44,8 +50,33 @@ public class Universidad {
     public ArrayList<Materia> getMaterias(){
         return materias;
     }
-    
+
     public void setMaterias(ArrayList<Materia> materias){
         this.materias = materias;
+    }
+
+    public int  getAlumnosId(){
+        return alumnosId;
+    }
+    public int  getDocentesId(){
+        return alumnosId;
+    }
+    public int  getPersonalId(){
+        return alumnosId;
+    }
+    public int  getMateriasId(){
+        return alumnosId;
+    }
+    public void setAlumnosId(int alumnosId){
+        this.alumnosId =  alumnosId;
+    }
+    public void setDocentesId(int docentesId){
+        this.docentesId =  docentesId;
+    }
+    public void setMateriasId(int materiasId){
+        this.materiasId =  materiasId;
+    }
+    public void setPersonalApoyoId(int personalApoyoId){
+        this.personalApoyoId =  personalApoyoId;
     }
 }
